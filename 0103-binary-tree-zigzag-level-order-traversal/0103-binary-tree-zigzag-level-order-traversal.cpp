cf9d8791b359cc7762d9452113b4c20df4c7e274
Vaibhav Dhanani <130571437+VaibhavDhanani@users.noreply.github.com>
2023-08-23 10:53:57 +0530

    Time: 0 ms (100%), Space: 12.1 MB (25.67%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
@@ -12,38 +12,30 @@
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-        vector<vector<int>> answer;
-        if (root == nullptr) return answer;
-        stack <TreeNode*> s1, s2;
-        s1.push(root);
-        while (!s1.empty() || !s2.empty()) {
-            vector <int> v;
-            while (!s1.empty()) {
-                TreeNode* temp = s1.top();
-                v.push_back(temp->val);
-                if (temp->left) s2.push(temp->left);
-                if (temp->right) s2.push(temp->right);
-                s1.pop();
-            }
-            if(v.size()!=0)
-            {
-                answer.push_back(v);
-                v.clear();
-            }
-           
-            while (!s2.empty()) {
-                TreeNode* temp2 = s2.top(); 
-                v.push_back(temp2->val);
-                if (temp2->right) s1.push(temp2->right);
-                if (temp2->left) s1.push(temp2->left);
-                s2.pop();
-            }
-            if(v.size()!=0)
-            {
-                answer.push_back(v);
-                v.clear();
-            }
-        }
-        return answer;
+       vector<vector<int>>ans;
+        if(root==NULL)return ans;
+       queue<TreeNode*>q;
+       q.push(root);
+       bool reverse=true;
+
+       while(!q.empty()){
+           int size=q.size();
+            vector<int>arr(size);
+           for(int i=0;i<size;i++){
+               TreeNode*temp=q.front();
+               q.pop();
+              int index=reverse?i:size-1-i;
+              arr[index]=temp->val;
+              if(temp->left){
+                  q.push(temp->left);
+              }
+               if(temp->right){
+                  q.push(temp->right);
+              }
+           }
+           reverse=!reverse;
+           ans.push_back(arr);
+       }  
+       return ans;
     }
 };
